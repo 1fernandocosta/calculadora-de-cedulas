c68558b7c9c5e3b1349c9cf9aad86b37dba0d9fb
1fernandocosta <fernando_250_@hotmail.com>
2022-12-09 22:58:11 -0300

    Testes input somente números

--- src/js/js.js
+++ src/js/js.js
@@ -9,6 +9,19 @@ function toLimit(string = ""){
    string.value = string.value.substring(0,6);
 }
 
+function somenteNumeros(e) {
+   let charCode = e.charCode ? e.charCode : e.keyCode;
+   // charCode 8 = backspace   
+   // charCode 9 = tab
+   if (charCode != 8 && charCode != 9) {
+       // charCode 48 equivale a 0   
+       // charCode 57 equivale a 9
+       if (charCode < 48 || charCode > 57) {
+           return false;
+       }
+   }
+}
+
 function somar() {
    let doisReais = parseInt(document.getElementById("dois-reais").value);
    if (isNaN(doisReais)) {
